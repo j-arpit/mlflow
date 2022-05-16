bb930db2e1c9c3a90e1ccb0bc7755e38f39d7a0b
Arpit Joshi <arpitjoshi333@gmail.com>
2022-05-16 12:23:02 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,2 @@
- #!/bin/bash
- echo "runid=f79243274b6d49909e0e40378b0f92e6" >> $GITHUB_ENV
- echo "port=6001" >> $GITHUB_ENV
+#!/bin/bash
+sudo -E /usr/local/bin/mlflow sagemaker run-local -m runs:/f79243274b6d49909e0e40378b0f92e6/model -i my-container -p 6001
