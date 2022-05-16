a3ebc09046a297abbb838b9c2949fa2ec6378c14
Arpit Joshi <arpitjoshi333@gmail.com>
2022-05-16 11:25:45 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
  #!/bin/bash
- sudo -E /usr/local/bin/mlflow sagemaker run-local -m runs:/f79243274b6d49909e0e40378b0f92e6/model -i my-container -p 5001
+ export runid = f79243274b6d49909e0e40378b0f92e6
+ export port = 5001
